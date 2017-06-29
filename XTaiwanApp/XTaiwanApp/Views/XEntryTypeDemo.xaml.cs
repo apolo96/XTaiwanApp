a823d5040f267bde2d69cb898f4977ded21ce329
apolo96 <andresfpvclap@gmail.com>
2017-06-29 11:36:14 -0500

    View Entry Type Demo

---
+++ XTaiwanApp/XTaiwanApp/Views/XEntryTypeDemo.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XTaiwanApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class XEntryTypeDemo : ContentPage
+	{
+		public XEntryTypeDemo ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Debug.WriteLine(e.NewTextValue);
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            Entry entry = sender as Entry;
+            Debug.WriteLine(entry.Text);
+        }
+    }
+}
